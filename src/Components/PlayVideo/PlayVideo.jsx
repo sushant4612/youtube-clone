99e04d5b48b907fec20fed69d51536a378962299
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-11-08 16:40:21 +0530

    Play video component added

---
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import './PlayVideo.css'
+import video1 from '../../assets/video.mp4'
+import like from '../../assets/like.png'
+import dislike from '../../assets/dislike.png'
+import share from '../../assets/share.png'
+import save from '../../assets/save.png'
+import jack from '../../assets/jack.png'
+import user_profile from '../../assets/user_profile.jpg'
+
+const PlayVideo = () => {
+  return (
+    <div className='play-video'>
+        <video src={video1} controls autoPlay muted></video>
+        <h3>Best Youtube Channel</h3>
+        <div className="play-video-info">
+            <p>1525 Views &bull; 2 days ago</p>
+            <div>
+                <span><img src={like} alt="" />125</span>
+                <span><img src={dislike} alt="" />2</span>
+                <span><img src={share} alt="" />Share</span>
+                <span><img src={save} alt="" />Save</span>
+            </div>
+        </div>
+        <hr />
+        <div className="publisher">
+            <img src={jack} alt="" />
+            <div>
+                <p>Sushant</p>
+                <span>1M Subscriber </span>         
+            </div>
+            <button>Subscribe</button>
+        </div>
+        <div className="vid-description">
+            <p>Channel that make some changes</p>
+            <p>Hey there subscribe to my youtube channel without forgotiing</p>
+            <hr />
+            <h4>130 Comments</h4>
+            <div className='comment'>
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>vaishali <span>1 day ago</span></h3>
+                    <p>A Strong bong of friendship be has devloped over the time which can't be broken by anyone</p>
+                    <div className="comment-action">
+                        <img src={like} alt="" />
+                        <span>244</span>
+                        <img src={dislike} alt="" />
+                    </div>
+                </div>
+            </div>
+            <div className='comment'>
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>vaishali <span>1 day ago</span></h3>
+                    <p>A Strong bong of friendship be has devloped over the time which can't be broken by anyone</p>
+                    <div className="comment-action">
+                        <img src={like} alt="" />
+                        <span>244</span>
+                        <img src={dislike} alt="" />
+                    </div>
+                </div>
+            </div>
+            <div className='comment'>
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>vaishali <span>1 day ago</span></h3>
+                    <p>A Strong bong of friendship be has devloped over the time which can't be broken by anyone</p>
+                    <div className="comment-action">
+                        <img src={like} alt="" />
+                        <span>244</span>
+                        <img src={dislike} alt="" />
+                    </div>
+                </div>
+            </div>
+            <div className='comment'>
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>vaishali <span>1 day ago</span></h3>
+                    <p>A Strong bong of friendship be has devloped over the time which can't be broken by anyone</p>
+                    <div className="comment-action">
+                        <img src={like} alt="" />
+                        <span>244</span>
+                        <img src={dislike} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default PlayVideo
---
+++ src/Components/Recommended/Recommended.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Recommended.css'
+
+const Recommended = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Recommended
